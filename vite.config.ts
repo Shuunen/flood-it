aab3d5f9518c137932859e8920796d78f17e1293
Romain Racamier-Lafon <romain.racamier@gmail.com>
2025-06-03 23:08:43 +0200

    fix: tailwind setup

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,11 @@
 import preact from '@preact/preset-vite'
+import tailwindcss from '@tailwindcss/vite'
 import { defineConfig } from 'vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [preact()],
+  plugins: [
+    preact(),
+    tailwindcss()
+  ],
 })
